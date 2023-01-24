34ee1c53f1e7be64eb179c2d5efa882fc5621cbb
agoodall <agoodall@workforcesoftware.com>
2023-01-24 08:21:47 +0000

    adding comments to explain whats happening.

--- cypress/page_objects/selecting_items.js
+++ cypress/page_objects/selecting_items.js
@@ -1,3 +1,7 @@
+//This page has the methods for selecting an item on a web page.
+
+
+//This method looks at a pageSelector that returns many values and indexes them to find the specified value.
 export function textToindexSeeker (pageSelector, text) {
     cy.get(pageSelector).each(($el, index) => {
     if ($el.text() === text) {
@@ -14,22 +18,29 @@ export function tdselectOnpage (pageSelector, text) {
         .click()
 }
 
+
+//This method looks for a defined pageSelector and clicks on it for you
 export function  clickAnitem(pageSelector) {
     cy.get(pageSelector).click()
 }
 
+
+//This method looks for a defined pageSelector, asserts it has the defined text then clicks on it for you
 export function hasTextandClick(pageSelector, text){
     cy.get(pageSelector).should('have.text', text).click()
 }
 
+//This method selects a value from a dropdown list
 export function dropdownSelect(pageSelector, value){
     cy.get(pageSelector).select(value)
 }
 
+//This method checks the checkbox where pageSelector has a type of checkbox
 export function checkboxCheck(pageSelector) {
     cy.get('[type="checkbox"]').check(pageSelector)
 }
 
+//This method unchecks the checkbox where pageSelector has a type of checkbox
 export function checkboxUncheck(pageSelector) {
     cy.get('[type="checkbox"]').uncheck(pageSelector)
 }
