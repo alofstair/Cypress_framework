07a1436d13371b73495840ada038698b649c683f
agoodall <agoodall@workforcesoftware.com>
2023-01-25 07:54:38 +0000

    Adding screen shot method

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,7 @@
 import * as itemSelect from '../page_objects/selecting_items.js'
 import * as itemDefine from '../page_objects/defining_items.js'
 import * as itemAssert from '../page_objects/asserting_items.js'
+import * as testRecording from "../page_objects/recording_tests"; '../page_objects/recording_tests.js'
 
 
 //Simple example
@@ -11,5 +12,6 @@ describe('Interacting with web elements', () => {
     //cy.visit('[Page I want to test]')
     //itemSelect.hasTextandClick('#id or other page selector', 'text I want to assert on')
     //itemAssert.hasText('#id or other page selector', 'text I want to assert on')
+    //testRecording.takeAscreenshot('Name I want to record screenshot as')
   })
 })
---
+++ cypress/page_objects/recording_tests.js
@@ -0,0 +1,3 @@
+export function takeAscreenshot(pageName) {
+    cy.screenshot(pageName)
+}
